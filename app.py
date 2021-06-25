f6c0609fa4283180190ac35db70172904f2e8f03
ZALCK <charbelzalk@gmail.com>
2021-06-25 16:51:55 +0000

    Database configuration with migrate

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
-from models import initDatabase
-from flask import render_template
+from models import db
 
 app = Flask(__name__)
 
+POSTGRES = {
+    'user': 'postgres',
+    'pw': 'root',
+    'db': 'timeManagement',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+db.init_app(app)
+
 @app.route('/')
 def index():
-    initDatabase()
     return "Hello world !"
 
 @app.route('/test')
--- models.py
+++ models.py
@@ -1,51 +1,42 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, Text, DateTime, ForeignKey
-from sqlalchemy.orm import declarative_base
-
+from flask_sqlalchemy import SQLAlchemy
 import logging as lg
 
-Base = declarative_base()
+db=SQLAlchemy()
+
 # Create database connection object
-engine = create_engine('postgresql+psycopg2://postgres:root@localhost/tutore')
+#engine = create_engine('postgresql+psycopg2://postgres:root@localhost/tutore')
 
-class Classe(Base):
-    __tablename__ = 't_classe'
-
-    id = Column(Integer, primary_key=True)
-    nom = Column(Text)
-    sessions = relationship("Session")
+class Classe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(200), nullable=False)
+    sessions = db.relationship('Session', backref='classe', lazy=True)
 
     def __init__(self, nom):
         self.nom = nom
 
         
-class Manager(Base):
-    __tablename__ = 't_manager'
-
-    id = Column(Integer, primary_key=True)
-    nom = Column(Text)
-    prenom = Column(Text)
+class Manager(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(200), nullable=False)
+    prenom = db.Column(db.String(200), nullable=False)
 
     def __init__(self, prenon, nom):
         self.nom = nom
         self.prenom = prenom
 
-class Session(Base):
-    __tablename__ = 't_session'
-
-    id = Column(Integer, primary_key=True)
-    nom = Column(Text)
-    fin = Column(DateTime)
-    debut = Column(DateTime)
-    classe_id = Column(Integer, ForeignKey('t_classe.id'))
+class Session(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(200), nullable=False)
+    fin = db.Column(db.DateTime)
+    debut = db.Column(db.DateTime)
+    classe_id = db.Column(db.Integer, db.ForeignKey('classe.id'), nullable=False)
     
     def __init__(self, nom, debut, fin ):
         self.nom =nom
         self.fin = fin
         self.debut = debut
 
-def initDatabase():
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
-    lg.warning('Database is ok')
+#def initDatabase():
+#    Base.metadata.drop_all(engine)
+#    Base.metadata.create_all(engine)
+#    lg.warning('Database is ok')
